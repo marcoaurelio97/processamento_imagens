4bb7ae4dd3bc646d87cfdef7058767aa8354ec71
Marco Aurélio <markinho9_7@hotmail.com>
2019-04-11 12:29:31 -0300

    projeto 3 finalizado

--- projeto3/main.py
+++ projeto3/main.py
@@ -8,11 +8,11 @@ def main():
 
     ret, thresh = cv2.threshold(img, 250, 255, cv2.THRESH_BINARY_INV)
 
-    thresh_filter = cv2.medianBlur(thresh, 5)
+    thresh_filter = cv2.medianBlur(thresh, 21)
 
     contours, hierarchy = cv2.findContours(thresh_filter, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-    cv2.drawContours(img, contours, -1, (255, 255, 255), 2)
+    cv2.drawContours(img_original, contours, -1, (255, 0, 0), 2)
 
     for contour in contours:
         x, y, w, h = cv2.boundingRect(contour)
@@ -33,6 +33,7 @@ def main():
     os.remove('dado_recortado.jpg')
 
     cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
 
 if __name__ == "__main__":
